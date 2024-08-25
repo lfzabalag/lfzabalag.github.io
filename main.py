04ca95401cba965654cbef977100842a756a7a6b
lfzabalag <lfzabalag@unal.edu.co>
2024-08-25 15:43:45 -0500

    metodo POST

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from flask import Flask, jsonify, request
 
-@app.route("<id>")
+afiliados = []
+#Rutas API:
+@app.route("/afiliados/<id>",methods=['GET','POST'])
 def get_afiliado(id):
-    afiliado = {"id":id,"nombres":nombre_test,"apellidos":apellido_test,"email":email_test,"estado":activo}
-    query = request.args.get('query')
-    if query:
-        afiliado["query"] = query
-    return jsonify(afiliado),200
+    if request.method == 'POST':
+        #Agregar un nuevo afiliado:
+        data = request.json
+        ingresa_afiliado = {
+            "id":len(afiliados) + 1, 
+            "nombres": data['nombres'],
+            "apellidos":data['apellidos'],
+            "email":data['email'],
+            "estado":data['estado']            
+        }
+        
+        afiliados.append(ingresa_afiliado)
+        return jsonify(data),201
+    
+    if request.method == 'GET':
+    #Recuperar lista de afiliados
+       return jsonify(afiliados),200
+
 
 app= Flask(__name__)
 if __name__=="__main__":
